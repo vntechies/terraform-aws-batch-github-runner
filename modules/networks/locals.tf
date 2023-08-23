ae278d231f2ca78df777024dd74f4d5ef3e0d6fd
kerashanog <kerashanog@gmail.com>
2023-08-23 21:14:04 +0700

    Remove unnecessary files

--- modules/networks/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  app_env_prefix = "${lookup(var.default_tags, "component", "-")}-${lookup(var.default_tags, "env", "-")}"
-}
--- modules/networks/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-    value = module.vpc["github-runner-batch"].vpc_id  
-}
-output "subnet_ids" {
-    value = [ for k, v in module.vpc["github-runner-batch"].subnet_ids : v ]
-}
-output "security_group" {
-    value = module.security_group.security_group_id
-}
