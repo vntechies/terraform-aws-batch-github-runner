5a5ae8d792015b07ecff277a0a8789cd5a753fee
kerashanog <kerashanog@gmail.com>
2023-08-28 20:35:34 +0700

    Add description to outputs

--- modules/secrets-manager/outputs.tf
+++ modules/secrets-manager/outputs.tf
@@ -1,7 +1,9 @@
 output "secret_id" {
+  description = "ID of Secret store in secret manager"
   value = aws_secretsmanager_secret.github_app.id
 }
 
 output "secret_arn" {
+  description = "ARN of Secret store in secret manager"
   value = aws_secretsmanager_secret.github_app.arn
 }
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,20 @@
 output "batch_job_definition" {
+  description = "A set of AWS Batch job definiton"
   value = module.batch.batch_job_definition
 }
 output "fargate_batch_job_queue" {
+  description = "Set of fargate batch job queue"
   value = module.batch.fargate_batch_job_queue
 }
 output "ec2_batch_job_queue" {
+  description = "Set of EC2 batch job queue"
   value = module.batch.ec2_batch_job_queue
 }
 output "secret_app_key" {
+  description = "ARN of the GitHub App Private key stored in Secret Manager"
   value = module.secrets_manager.secret_arn
 }
 output "api_gateway_invoke_url" {
+  description = "URL of API Gateway Webhook, use to set in GitHub App webhook"
   value = module.api_gateway.api_invoke_url
 }
